42f502b527719ad736fdf182cf0ddb4f1819ebf9
mxnan <128995981+mxnan@users.noreply.github.com>
2024-05-15 12:49:24 +0530

    :sparkles:
    :sparkles: added mdx support for content .
    :sparkles: allBlogs fetching all details

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,17 +1,17 @@
 import { defineCollection, z } from "astro:content";
 
 const blogCollection = defineCollection({
- type: "content",
- schema: z.object({
-   title: z.string(),
-   author: z.string(),
-   description: z.string(),
-   pubDate: z.date(),
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    author: z.string(),
+    description: z.string(),
+    pubDate: z.date(),
+    ogimage: z.string(),
    
- })
+  }),
 });
 
 export const collections = {
   blog: blogCollection,
 };
-
