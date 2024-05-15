5854edd6a45f7ddac45f6bdba366aa5548da0f60
mxnan <128995981+mxnan@users.noreply.github.com>
2024-05-15 20:51:15 +0530

    :boom:
    :sparkles: added frontmatter and pageTitle props to baselayout.
    :sparkles: using frontmatter and pageTitle on every page .
    :sparkles: created frontmatter type in constants.ts
    :sparkles: slug page dynamically entering frontmatter and pageTitle

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,11 @@
 export const SITE_TITLE = "mxnan.dev";
 export const SITE_DESCRIPTION = "Tech Blog and Portfolio of @mxnan";
+
+
+// Define an interface for the frontmatter properties
+export interface Frontmatter {
+  title: string;
+  description: string;
+  ogImage: string;
+  author: string;
+}
