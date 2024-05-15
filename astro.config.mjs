a981aabeec343841c2a982596da52f8f7510541a
mxnan <128995981+mxnan@users.noreply.github.com>
2024-05-15 13:47:58 +0530

    :sparkles:
    :sparkles: added ...slug page in blogs using SSR.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+  integrations: [
+    tailwind(),
+    mdx({
+      syntaxHighlight: "shiki",
+      shikiConfig: { theme: "one-dark-pro" },
+    }),
+  ],
+  output: "server",
 });
