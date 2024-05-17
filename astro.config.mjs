bf64ed1a308ff4aeaa6fe855d2bb649e7b66281b
mxnan <128995981+mxnan@users.noreply.github.com>
2024-05-17 23:47:34 +0530

    :hammer:
    :heavy_plus_sign: fixed font by adding font to headtag and using in tailwind config.
    :heavy_plus_sign: removed globals.css .
    :heavy_plus_sign: adjusted all pages and components.
    :heavy_plus_sign: adjusted readme.md
    :heavy_plus_sign: added prose classes to style

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,9 +8,7 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   integrations: [
     tailwind({
-      // Example: Disable injecting a basic `base.css` import on every page.
-      // Useful if you need to define and/or import your own custom `base.css`.
-      applyBaseStyles: false,
+      config: { applyBaseStyles: false },
     }),
     mdx({
       syntaxHighlight: "shiki",
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,14 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    fontFamily: {
-      body: ["var(--font-body)"],
-      title: ["var(--font-title)"],
+    extend: {
+      fontFamily: {
+        sans: ["Montserrat", ...defaultTheme.fontFamily.sans],
+      },
     },
-    extend: {},
   },
   plugins: [require("@tailwindcss/typography")],
 };
